9acc4db17c97008538d10bc6973504280a47c5da
MongiLearnsToCode <sandiledlmn@gmail.com>
2025-10-18 13:45:34 +0200

    Reduce mobile padding for auth components to maximize space

--- pages/SignUpPage.tsx
+++ pages/SignUpPage.tsx
@@ -6,7 +6,7 @@ export const SignUpPage: React.FC = () => {
   const { navigateTo } = useApp();
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-[60vh] px-4 sm:px-6 lg:px-8 py-8">
+    <div className="flex flex-col justify-center items-center min-h-[60vh] px-2 sm:px-6 lg:px-8 py-8">
       <div className="w-full max-w-md">
         <SignUp 
           routing="virtual"
